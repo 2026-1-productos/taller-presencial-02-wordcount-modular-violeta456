b60dd6da51d6869b09702c80cebb8b6be8825939
violeta <159485730+violeta456@users.noreply.github.com>
2026-05-16 23:35:30 -0500

    cambio test. Taller 02 Completo

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -21,7 +21,7 @@ def test_homework():
 
     try:
         subprocess.run(
-            ["python3", "-m", "homework", "data/input", "data/output"],
+            ["python", "-m", "homework", "data/input", "data/output"],
             check=True,
         )
     except subprocess.CalledProcessError as e:
